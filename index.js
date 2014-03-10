eee66ae716fe3ed6d9c3f89facd6798773497f4a
Oleg Seletsky <oleg.b.seletsky@gmail.com>
2014-03-10 11:27:43 -0700

    bug fix

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ var sass = require( "node-sass" );
 var path = require( "path" );
 
 module.exports = function( file ) {
+	var data = "";
 	if( file !== undefined && path.extname( file ) !== ".scss" )
 		return through();
 	else
 		return through( write, end );
-
-	var data = "";
 
 	function write(buf) {
 		data += buf;
